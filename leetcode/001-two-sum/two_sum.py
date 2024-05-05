18c274644d9f88fe38052a85075c543dbe11e2d3
fritzrehde <fritzrehde.work@gmail.com>
2024-05-05 12:35:31 +1000

    leetcode(001-two-sum): refactored

--- leetcode/001-two-sum/two_sum.py
+++ leetcode/001-two-sum/two_sum.py
@@ -7,60 +7,67 @@
 # Given an array of integers `nums` and an integer `target`, return indices of
 # the two numbers such that they add up to `target`.
 
+# Run `pytest two_sum.py`.
 def test():
-    # specify approach
-    two_sum = Solution().one_pass
+    def test_algo(algo):
+        # A[0] + A[1] = 2 + 7 = 9
+        assert algo([2, 7, 11, 15], 9) == [0, 1]
 
-    # pytest
-    assert two_sum([2,7,11,15], 9) == [0,1]
-    assert two_sum([3,2,4], 6) == [1,2]
-    assert two_sum([3,3], 6) == [0,1]
+        # A[1] + A[2] = 2 + 4 = 6
+        assert algo([3, 2, 4], 6) == [1, 2]
+
+        # A[0] + A[1] = 3 + 3 = 6
+        assert algo([3, 3], 6) == [0, 1]
+
+        # Edge case: can't sum up multiple numbers in a one-element array
+        assert algo([42], 42) == None
+
+        # Edge case: can't sum up multiple numbers in a empty array
+        assert algo([], 42) == None
+
+    # Test all different algorithms/implementations
+    solution = Solution()
+    for algo in [solution.brute_force, solution.one_pass]:
+        test_algo(algo)
+
 
 class Solution:
-    """
-    Approach:  Naive/brute-force solution
-    Idea:      For each number, check if addition with any other number equals `target`
-    Time:      O(n^2), for each element we loop through entire list
-    Space:     O(1), no allocations
-    Leetcode:  3653 ms runtime, 14.9 MB memory
-    """
-    def naive(self, nums: list[int], target: int) -> list[int]:
-        ln = len(nums)
-        for i in range(ln):
-            for j in range(i+1, ln):
+    def brute_force(self, nums: list[int], target: int) -> list[int]:
+        """
+        Approach:  Brute-force
+        Idea:      For each number, check if addition with any other number equals `target`
+        Time:      O(n^2), compare each element to each other element
+        Space:     O(1), no allocations
+        Leetcode:  3653 ms runtime, 14.9 MB memory
+        """
+
+        len_ = len(nums)
+        for i in range(len_):
+            for j in range(i+1, len_):
                 if (nums[i] + nums[j] == target):
                     return [i, j]
 
-    """
-    Approach:  Two-pass with dictionary
-    Idea:      Create a dictionary storing the complements of each list number, and then find complement
-    Time:      O(2n) -> O(n), list is traversed twice, once to build dict and once to find complement
-    Space:     O(n), space for dictionary
-    Leetcode:  62 ms runtime, 15.4 MB memory
-    """
-    def two_pass(self, nums: list[int], target: int) -> list[int]:
-        # create dict of the complements (value and index) of each number
-        complements = {target-num:i for i,num in enumerate(nums)}
+        return None
 
-        # find position for which complement exists in list, filter out same index
-        for i, num in enumerate(nums):
-            if i != complements.get(num, i):
-                return [i, complements[num]]
-
-    """
-    Approach:  One-pass with dictionary
-    Idea:      Only check if compliment for a value has already been seen
-    Time:      O(n), list is traversed once (second traversal is avoided through faster dict.get())
-    Space:     O(n), space for dictionary
-    Leetcode:  51 ms runtime, 15.1 MB memory
-    """
     def one_pass(self, nums: list[int], target: int) -> list[int]:
-        complements = dict()
+        """
+        Approach:  One-pass with dictionary
+        Idea:      Only check if complement for a value has already been seen
+        Time:      O(n), list is traversed once (second traversal is avoided through faster index lookup given number)
+        Space:     O(n), space for hashmap
+        Leetcode:  51 ms runtime, 15.1 MB memory
+        """
+        # Map each number to index in array
+        nums_indices = dict()
+
         for i, num in enumerate(nums):
             complement = target - num
 
-            # check if complement has already been seen
-            if (j := complements.get(complement)) is not None:
-                return [j, i]
+            # Get the complement's index if it has already been seen
+            if (complement_idx := nums_indices.get(complement)) is not None:
+                # Since the complement was seen before this ith element num, it must appear in the array earlier than num.
+                return [complement_idx, i]
             else:
-                complements[num] = i
+                nums_indices[num] = i
+
+        return None
