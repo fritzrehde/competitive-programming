05ed740b2cb0cd7848f1e61ca73ac364474fd550
fritzrehde <fritzrehde.work@gmail.com>
2024-05-08 14:28:53 +1000

    feat: read leetcode problem from editor in script

--- leetcode/setup_problem.py
+++ leetcode/setup_problem.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python3
 
-# Given the URL of a leetcode problem, setup the template directories, files and additions to the README.
+# Given details of a leetcode problem, setup the template directories, files and additions to the README.
 
 import os
 import re
 import textwrap
+import tempfile
+import subprocess
+import itertools
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
+# TODO: prepare the git commit message as well
+
 
 def create_leetcode_problem():
+    def input_editor(prompt: str) -> str:
+        with tempfile.NamedTemporaryFile(mode='r+', delete=True) as f:
+            # Ensure prompt is written to disk before opening editor.
+            f.write(prompt)
+            f.flush()
+
+            # Open the editor
+            editor = os.getenv('EDITOR', 'vim')
+            subprocess.call([editor, f.name])
+
+            # The editor leaves the file pointer at EOF, so move back to start.
+            f.seek(0)
+            return f.read()
+
     # Prompting user for inputs
     problem_url = input("Enter the problem URL: ")
     problem_id = input("Enter the problem identifier: ")
     problem_title = input("Enter the problem title: ")
     problem_difficulty = input("Enter the problem difficulty: ")
-    problem_description = input("Enter the text describing the problem: ")
+    problem_description = input_editor(
+        "Replace this line with the text describing the problem.\n")
 
     # Make the difficulty title-case.
     problem_difficulty = problem_difficulty.title()
 
-    # Remove `description/` or `description` from end of URL.
-    remove_from_end = ["description/", "description"]
-    for end in remove_from_end:
-        if problem_url.endswith(end):
-            # Remove the last len(end) characters.
-            problem_url = problem_url[:-len(end)]
-            break
+    # Only keep `https://leetcode.com/problems/<problem-name>/`, and remove anything after.
+    match = re.match(r"(https://leetcode.com/problems/[^/]+/)", problem_url)
+    if match:
+        problem_url = match.group(0)
+    else:
+        raise RuntimeError("failed to parse leetcode problem url")
 
     # Convert problem number to 4 digit number, filling unused digit spaces with zeros.
     digits = 4
@@ -42,10 +61,22 @@ def create_leetcode_problem():
     dir_name = f"{problem_id_filled}-{"-".join(problem_title_iter)}"
     file_name = f"{"_".join(problem_title_iter)}.py"
 
-    # Split the description into chunks of at most 80-2=78 (since "# " starts each line).
+    # Split each line of the description into chunks of at most 80-2=78 (since "# " starts each line).
     wrapper = textwrap.TextWrapper(
         width=78, break_long_words=False, replace_whitespace=False)
-    problem_description = wrapper.wrap(problem_description)
+    problem_description_lines = []
+    for line in problem_description.split('\n'):
+        # problem_description_lines.extend(wrapper.wrap(line))
+        wrapped = wrapper.wrap(line)
+        if wrapped:
+            problem_description_lines.extend(wrapped)
+        else:
+            # Append an empty string to represent an empty line.
+            problem_description_lines.append("")
+
+    # Remove the last description line if it was an empty newline.
+    if problem_description_lines[-1] == "":
+        problem_description_lines.pop()
 
     def find_readme_line_to_insert(readme: list[str], new_problem_id: int) -> int:
         # Find index of leetcode section
@@ -96,7 +127,7 @@ def create_leetcode_problem():
 #
 # {problem_url}
 #
-# {"\n# ".join(problem_description)}
+# {"\n# ".join(problem_description_lines)}
 
 
 def test():
